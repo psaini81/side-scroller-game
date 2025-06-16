864f47e47edcf9469b7f8367c2e1b794427d66b8
psaini81 <77945555+psaini81@users.noreply.github.com>
2025-06-16 14:27:01 +1000

    Adding Mouse and player 1 images. I have also added another 3 lines in the main code.

--- side_scroller_game.py
+++ side_scroller_game.py
@@ -1,3 +1,12 @@
-class Base:
-    def __init__(self):
+import pygame
+import sys
+
+# Pygame setup
+pygame.init()
+WIDTH, HEIGHT = 800, 400
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Pixel Battle")
+CLOCK = pygame.time.Clock()
+
+
         
